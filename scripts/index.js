2b3278c6a8598b5e2c090d505d55e55fe25d2d03
AnastasiyaSavelyeva <savelas22@yandex.ru>
2023-05-10 19:08:33 +0300

    correcting modal

--- scripts/index.js
+++ scripts/index.js
@@ -1,185 +1,193 @@
-/*$(document).ready(function(){
-    $('.slider-inner').slick({
-        arrows: true,
-        prevArrow: '.arrow-prev',
-        nextArrow: '.arrow-next',
-        dots: false,
-        dotsClass: 'slick-dots',
-        infinite: true,
-        speed: 500,
-        fade: true,
-        cssEase: 'linear',
-        autoplay: true,
-});
-});*/
+'use strict'
+$(document).ready(function() {
 
-new WOW({
-    animateClass: 'animate__animated'
-}).init();
+    /*$(document).ready(function(){
+        $('.slider-inner').slick({
+            arrows: true,
+            prevArrow: '.arrow-prev',
+            nextArrow: '.arrow-next',
+            dots: false,
+            dotsClass: 'slick-dots',
+            infinite: true,
+            speed: 500,
+            fade: true,
+            cssEase: 'linear',
+            autoplay: true,
+    });
+    });*/
 
-function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-}
+    new WOW({
+        animateClass: 'animate__animated'
+    }).init();
 
-function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-}
+    function openNav() {
+        document.getElementById("myNav").style.width = "100%";
+    }
+
+    function closeNav() {
+        document.getElementById("myNav").style.width = "0%";
+    }
 
 
 //progress-bar
-/*
-window.onscroll = function() {myFunction()};
+    /*
+    window.onscroll = function() {myFunction()};
 
-function myFunction() {
-    const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-    const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-    const scrolled = (winScroll / height) * 100;
-    document.getElementById("myBar").style.width = scrolled + "%";
-}*/
+    function myFunction() {
+        const winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+        const height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+        const scrolled = (winScroll / height) * 100;
+        document.getElementById("myBar").style.width = scrolled + "%";
+    }*/
 
 //drop-menu
 
-const dropMenu = document.getElementById('drop-menu');
-const list = document.getElementById('drop-list');
+    const dropMenu = document.getElementById('drop-menu');
+    const list = document.getElementById('drop-list');
 
-dropMenu.addEventListener('click', () => {
-    list.style.display === 'block' ? list.style.display = 'none' : list.style.display = 'block';
-})
+    dropMenu.addEventListener('click', () => {
+        list.style.display === 'block' ? list.style.display = 'none' : list.style.display = 'block';
+    })
 
-window.addEventListener('click', e => {
-    const target = e.target
-    if (!target.closest('#drop-list') && !target.closest('#drop-menu')) {
-        list.style.display = 'none'
-    }
-})
+    window.addEventListener('click', e => {
+        const target = e.target
+        if (!target.closest('#drop-list') && !target.closest('#drop-menu')) {
+            list.style.display = 'none'
+        }
+    })
 
-/*//input-mask
-const phone = document.getElementById('phone')
-phone.inputmask({"mask": "+7 (999) 999-99-99"});*/
+    /*//input-mask
+    const phone = document.getElementById('phone')
+    phone.inputmask({"mask": "+7 (999) 999-99-99"});*/
 
 //popup
 
-const popupButton = document.getElementById('popup-button');
-const closeButton = document.getElementById('close')
-const popup = document.getElementById('popup');
+    const popupButton = document.getElementById('popup-button');
+    const closeButton = document.getElementById('close')
+    const popup = document.getElementById('popup');
 
-popupButton.addEventListener('click', () => {
-    popup.style.display = 'flex';
-})
+    popupButton.addEventListener('click', () => {
+        popup.style.display = 'flex';
+    })
 
-closeButton.addEventListener('click', () => {
-    popup.style.display = 'none';
-})
+    closeButton.addEventListener('click', () => {
+        popup.style.display = 'none';
+    })
 
 //scrolling
 
 
-$('#about').on('click', function(e) {
-    e.preventDefault();
-    $('.about')[0].scrollIntoView({behavior: "smooth"});
-})
+    $('#about').on('click', function (e) {
+        e.preventDefault();
+        $('.about')[0].scrollIntoView({behavior: "smooth"});
+    })
 
-$('#contacts').on('click', function(e) {
-    e.preventDefault();
-    $('.contacts')[0].scrollIntoView({behavior: "smooth"});
-})
+    $('#contacts').on('click', function (e) {
+        e.preventDefault();
+        $('.contacts')[0].scrollIntoView({behavior: "smooth"});
+    })
 
 //collapse
 
-const coll = document.getElementsByClassName("collapsible");
+    const coll = document.getElementsByClassName("collapsible");
 
-for (let i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        const content = this.nextElementSibling;
-        if (content.style.maxHeight){
-            content.style.maxHeight = null;
-            content.style.padding = '0px 18px';
-        } else {
-            content.style.maxHeight = content.scrollHeight + "px";
-            content.style.padding = '15px';
-        }
-    });
-}
+    for (let i = 0; i < coll.length; i++) {
+        coll[i].addEventListener("click", function () {
+            this.classList.toggle("active");
+            const content = this.nextElementSibling;
+            if (content.style.maxHeight) {
+                content.style.maxHeight = null;
+                content.style.padding = '0px 18px';
+            } else {
+                content.style.maxHeight = content.scrollHeight + "px";
+                content.style.padding = '15px';
+            }
+        });
+    }
 
 
 //main
 
-$(document).ready(function($){
-    // проверка является ли .cd-image-container в области видимости
-    // ...
+    $(document).ready(function ($) {
+        // проверка является ли .cd-image-container в области видимости
+        // ...
 
-    // делаем элемент .cd-handle движимым и сменяем позицию .cd-resize-img
-    $('.cd-image-container').each(function(){
-        var actual = $(this);
-        drags(actual.find('.cd-handle'), actual.find('.cd-resize-img'), actual);
+        // делаем элемент .cd-handle движимым и сменяем позицию .cd-resize-img
+        $('.cd-image-container').each(function () {
+            var actual = $(this);
+            drags(actual.find('.cd-handle'), actual.find('.cd-resize-img'), actual);
+        });
     });
-});
 
 // реализация перетаскивание http://css-tricks.com/snippets/jquery/draggable-without-jquery-ui/
-function drags(dragElement, resizeElement, container) {
-    dragElement.on("mousedown vmousedown", function(e) {
-        dragElement.addClass('draggable');
-        resizeElement.addClass('resizable');
+    function drags(dragElement, resizeElement, container) {
+        dragElement.on("mousedown vmousedown", function (e) {
+            dragElement.addClass('draggable');
+            resizeElement.addClass('resizable');
 
-        let dragWidth = dragElement.outerWidth(),
-            xPosition = dragElement.offset().left + dragWidth - e.pageX,
-            containerOffset = container.offset().left,
-            containerWidth = container.outerWidth(),
-            minLeft = containerOffset + 10,
-            maxLeft = containerOffset + containerWidth - dragWidth - 10;
+            let dragWidth = dragElement.outerWidth(),
+                xPosition = dragElement.offset().left + dragWidth - e.pageX,
+                containerOffset = container.offset().left,
+                containerWidth = container.outerWidth(),
+                minLeft = containerOffset + 10,
+                maxLeft = containerOffset + containerWidth - dragWidth - 10;
 
-        dragElement.parents().on("mousemove vmousemove", function(e) {
-            let leftValue = e.pageX + xPosition - dragWidth;
+            dragElement.parents().on("mousemove vmousemove", function (e) {
+                let leftValue = e.pageX + xPosition - dragWidth;
 
-            if(leftValue < minLeft ) {
-                leftValue = minLeft;
-            } else if ( leftValue > maxLeft) {
-                leftValue = maxLeft;
-            }
+                if (leftValue < minLeft) {
+                    leftValue = minLeft;
+                } else if (leftValue > maxLeft) {
+                    leftValue = maxLeft;
+                }
 
-            let widthValue = (leftValue + dragWidth/2 - containerOffset)*100/containerWidth+'%';
+                let widthValue = (leftValue + dragWidth / 2 - containerOffset) * 100 / containerWidth + '%';
 
-            $('.draggable').css('left', widthValue).on("mouseup vmouseup", function() {
-                $(this).removeClass('draggable');
+                $('.draggable').css('left', widthValue).on("mouseup vmouseup", function () {
+                    $(this).removeClass('draggable');
+                    resizeElement.removeClass('resizable');
+                });
+
+                $('.resizable').css('width', widthValue);
+
+                // ...
+
+            }).on("mouseup vmouseup", function (e) {
+                dragElement.removeClass('draggable');
                 resizeElement.removeClass('resizable');
             });
-
-            $('.resizable').css('width', widthValue);
-
-            // ...
-
-        }).on("mouseup vmouseup", function(e){
+            e.preventDefault();
+        }).on("mouseup vmouseup", function (e) {
             dragElement.removeClass('draggable');
             resizeElement.removeClass('resizable');
         });
-        e.preventDefault();
-    }).on("mouseup vmouseup", function(e) {
-        dragElement.removeClass('draggable');
-        resizeElement.removeClass('resizable');
-    });
-}
+    }
 
 //scroll-button
 // Получить кнопку:
-mybutton = document.getElementById("myBtn");
+    const myButton = document.getElementById("myBtn");
 
 // Когда пользователь прокручивает вниз 20px от верхней части документа, покажите кнопку
-window.onscroll = function() {scrollFunction()};
+    window.onscroll = function () {
+        scrollFunction()
+    };
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            myButton.style.display = "block";
+        } else {
+            myButton.style.display = "none";
+        }
     }
-}
-// Когда пользователь нажимает на кнопку, прокрутите до верхней части документа
-function topFunction() {
 
-    document.body.scrollTop = 0; // Для Safari
-    document.documentElement.scrollTop = 0; // Для Chrome, Firefox, IE и Opera
-}
+// Когда пользователь нажимает на кнопку, прокрутите до верхней части документа
+    function topFunction() {
+
+        document.body.scrollTop = 0; // Для Safari
+        document.documentElement.scrollTop = 0; // Для Chrome, Firefox, IE и Opera
+    }
+
+})
 
 
 
--- scripts/services.js
+++ scripts/services.js
@@ -1,146 +1,157 @@
-new WOW({
-    animateClass: 'animate__animated'
-}).init();
+'use strict'
 
-function openNav() {
-    document.getElementById("myNav").style.width = "100%";
-}
 
-function closeNav() {
-    document.getElementById("myNav").style.width = "0%";
-}
+$(document).ready(function() {
+
+    new WOW({
+        animateClass: 'animate__animated'
+    }).init();
+
+    function openNav() {
+        document.getElementById("myNav").style.width = "100%";
+    }
+
+    function closeNav() {
+        document.getElementById("myNav").style.width = "0%";
+    }
 
 //show examples
 
-const showButtonOne = document.getElementById('show-examples-one');
-showButtonOne.addEventListener('click', () => {
-    const content = document.getElementById('example-one');
-    const state = content.style.display;
-    if (state === 'grid') content.style.display='none';
-    else content.style.display = 'grid';
-})
+    const showButtonOne = document.getElementById('show-examples-one');
+    showButtonOne.addEventListener('click', () => {
+        const content = document.getElementById('example-one');
+        const state = content.style.display;
+        if (state === 'grid') content.style.display = 'none';
+        else content.style.display = 'grid';
+    })
 
-const showButtonTwo = document.getElementById('show-examples-two');
-showButtonTwo.addEventListener('click', () => {
-    const content = document.getElementById('example-two');
-    const state = content.style.display;
-    if (state === 'grid') content.style.display='none';
-    else content.style.display = 'grid';
-})
+    const showButtonTwo = document.getElementById('show-examples-two');
+    showButtonTwo.addEventListener('click', () => {
+        const content = document.getElementById('example-two');
+        const state = content.style.display;
+        if (state === 'grid') content.style.display = 'none';
+        else content.style.display = 'grid';
+    })
 
-const showButtonThree = document.getElementById('show-examples-three');
-showButtonThree.addEventListener('click', () => {
-    const content = document.getElementById('example-three');
-    const state = content.style.display;
-    if (state === 'grid') content.style.display='none';
-    else content.style.display = 'grid';
-})
+    const showButtonThree = document.getElementById('show-examples-three');
+    showButtonThree.addEventListener('click', () => {
+        const content = document.getElementById('example-three');
+        const state = content.style.display;
+        if (state === 'grid') content.style.display = 'none';
+        else content.style.display = 'grid';
+    })
 
-const showButtonFour = document.getElementById('show-examples-four');
-showButtonFour.addEventListener('click', () => {
-    const content = document.getElementById('example-four');
-    const state = content.style.display;
-    if (state === 'grid') content.style.display='none';
-    else content.style.display = 'grid';
-})
+    const showButtonFour = document.getElementById('show-examples-four');
+    showButtonFour.addEventListener('click', () => {
+        const content = document.getElementById('example-four');
+        const state = content.style.display;
+        if (state === 'grid') content.style.display = 'none';
+        else content.style.display = 'grid';
+    })
 
-const showButtonFive = document.getElementById('show-examples-five');
-showButtonFive.addEventListener('click', () => {
-    const content = document.getElementById('example-five');
-    const state = content.style.display;
-    if (state === 'grid') content.style.display='none';
-    else content.style.display = 'grid';
-})
+    const showButtonFive = document.getElementById('show-examples-five');
+    showButtonFive.addEventListener('click', () => {
+        const content = document.getElementById('example-five');
+        const state = content.style.display;
+        if (state === 'grid') content.style.display = 'none';
+        else content.style.display = 'grid';
+    })
 
-const showButtonSix = document.getElementById('show-examples-six');
-showButtonSix.addEventListener('click', () => {
-    const content = document.getElementById('example-six');
-    const state = content.style.display;
-    if (state === 'grid') content.style.display='none';
-    else content.style.display = 'grid';
-})
+    const showButtonSix = document.getElementById('show-examples-six');
+    showButtonSix.addEventListener('click', () => {
+        const content = document.getElementById('example-six');
+        const state = content.style.display;
+        if (state === 'grid') content.style.display = 'none';
+        else content.style.display = 'grid';
+    })
 
-const showButtonSeven = document.getElementById('show-examples-seven');
-showButtonSeven.addEventListener('click', () => {
-    const content = document.getElementById('example-seven');
-    const state = content.style.display;
-    if (state === 'grid') content.style.display='none';
-    else content.style.display = 'grid';
-})
+    const showButtonSeven = document.getElementById('show-examples-seven');
+    showButtonSeven.addEventListener('click', () => {
+        const content = document.getElementById('example-seven');
+        const state = content.style.display;
+        if (state === 'grid') content.style.display = 'none';
+        else content.style.display = 'grid';
+    })
 
-const showButtonEight = document.getElementById('show-examples-eight');
-showButtonEight.addEventListener('click', () => {
-    const content = document.getElementById('example-eight');
-    const state = content.style.display;
-    if (state === 'grid') content.style.display='none';
-    else content.style.display = 'grid';
-})
+    const showButtonEight = document.getElementById('show-examples-eight');
+    showButtonEight.addEventListener('click', () => {
+        const content = document.getElementById('example-eight');
+        const state = content.style.display;
+        if (state === 'grid') content.style.display = 'none';
+        else content.style.display = 'grid';
+    })
 
 
 //popup-open-image
-$('.popup-open-image').magnificPopup({
-    type: 'image',
-    gallery:{enabled:true},
-    closeBtnInside: true
-});
-
+    $('.popup-open-image').magnificPopup({
+        type: 'image',
+        gallery: {enabled: true},
+        closeBtnInside: true
+    });
 
 
 //more
-const modal = document.getElementById("myModal");
-const modalTwo = document.getElementById("myModalTwo");
+    const modal = document.getElementById("myModal");
+    const modalTwo = document.getElementById("myModalTwo");
 
 // Get the button that opens the modal
-const btn = document.getElementById("moreButton");
-const btnTwo = document.getElementById("moreButtonTwo");
+    const btn = document.getElementById("moreButton");
+    const btnTwo = document.getElementById("moreButtonTwo");
 
 // Get the <span> element that closes the modal
-const span = document.getElementsByClassName("close")[0];
-const spanTwo = document.getElementsByClassName("close")[1];
+    const span = document.getElementsByClassName("closeModal")[0];
+    const spanTwo = document.getElementsByClassName("closeModal")[1];
 
 // When the user clicks the button, open the modal
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-btnTwo.onclick = function() {
-    modalTwo.style.display = "block";
-}
+    btn.onclick = function () {
+        modal.style.display = "block";
+    }
+    btnTwo.onclick = function () {
+        modalTwo.style.display = "block";
+    }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-    modal.style.display = "none";
-}
-spanTwo.onclick = function() {
-    modalTwo.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target === modal || event.target === modalTwo) {
+    span.onclick = function () {
         modal.style.display = "none";
     }
-}
+    spanTwo.onclick = function () {
+        modalTwo.style.display = "none";
+    }
+
+// When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target === modal || event.target === modalTwo) {
+            modal.style.display = "none";
+        }
+    }
+
+})
 
 //scroll-button
 // Получить кнопку:
-mybutton = document.getElementById("myBtn");
+   const myButton = document.getElementById("myBtn");
 
 // Когда пользователь прокручивает вниз 20px от верхней части документа, покажите кнопку
-window.onscroll = function() {scrollFunction()};
+    window.onscroll = function () {
+        scrollFunction()
+    };
 
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        mybutton.style.display = "block";
-    } else {
-        mybutton.style.display = "none";
+    function scrollFunction() {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            myButton.style.display = "block";
+        } else {
+            myButton.style.display = "none";
+        }
     }
-}
-// Когда пользователь нажимает на кнопку, прокрутите до верхней части документа
-function topFunction() {
 
-    document.body.scrollTop = 0; // Для Safari
-    document.documentElement.scrollTop = 0; // Для Chrome, Firefox, IE и Opera
-}
+// Когда пользователь нажимает на кнопку, прокрутите до верхней части документа
+    function topFunction() {
+
+        document.body.scrollTop = 0; // Для Safari
+        document.documentElement.scrollTop = 0; // Для Chrome, Firefox, IE и Opera
+    }
+
+
 
 
 
